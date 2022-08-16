deb76bebe326235b20c20891cdbcf42d52118219
Rafe786 <110394841+Rafe786@users.noreply.github.com>
2022-08-16 04:43:20 -0700

    Add files via upload

---
+++ ValueParameterSwap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgranToUpload
+{
+    class ValueParameterSwap
+    {
+        static void Swapping(int x, int y)
+        {
+            Console.WriteLine(x+ "  " + y);
+            Console.WriteLine("==========Star Swapping==========");
+            int z = x;
+            x = y;
+            y = z;
+            Console.WriteLine(x+ "  " + y);
+            Console.WriteLine("==========End Swapping==========");
+        }
+        static void Main()
+        {
+            int num1 = 10;
+            int num2 = 200;
+            Console.WriteLine(num1+ "  "+ num2);
+            Swapping(num1, num2);
+            Console.ReadLine();
+        }
+    }
+}
