eb3198c890d16874be0178faf5ffabe1db605f15
Rafe786 <110394841+Rafe786@users.noreply.github.com>
2022-08-18 05:05:23 -0700

    Add files via upload

---
+++ NamedParameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgranToUpload
+{
+    class NamedParameter
+    {
+        static void Display(int empNo, string empName, double empSal)
+        {
+             Console.WriteLine(empNo);
+            Console.WriteLine(empName);
+            Console.WriteLine(empSal);
+         }
+        static void Main()
+        {
+            Display(empSal:21056.36,empNo:21,empName:"Rafe");
+            Console.ReadLine();
+        }
+    }
+}
