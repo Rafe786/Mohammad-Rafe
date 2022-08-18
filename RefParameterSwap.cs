6c470e7136ed910be858f02657454169f403887c
Rafe786 <110394841+Rafe786@users.noreply.github.com>
2022-08-18 05:07:30 -0700

    Add files via upload

---
+++ RefParameterSwap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgranToUpload
+{
+    class RefParameterSwap
+    {
+        static void SwapValue(ref int x, ref int y)
+        {
+            Console.WriteLine(x+ "  "+ y);
+            Console.WriteLine("------------------Start Swapping------------------");
+            int z = x;
+            x = y;
+            y = z;
+            Console.WriteLine(x+ "  " + y);
+            Console.WriteLine("------------------End Swapping---------------------");
+        }
+        static void Main()
+        {
+            int num1 = 1;
+            int num2 = 500;
+           
+            SwapValue(ref num1, ref num2);
+            Console.WriteLine(num1 + "  " + num2);
+            Console.ReadLine();
+        }
+    }
+}
