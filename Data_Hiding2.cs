1161f1902288855a3a6c6133ad70fab9020d0ca8
Rafe786 <110394841+Rafe786@users.noreply.github.com>
2022-08-23 05:38:46 -0700

    Update Data_Hiding2.cs

--- Data_Hiding2.cs
+++ Data_Hiding2.cs
@@ -1,15 +1,15 @@
-﻿class Demo3
+﻿class Demo
 {
     public static int num = 100;
 }
-class Test1 : Demo3
+class Test : Demo
 {
     new public static int num = 200;   //Data Hiding .........
 
-    static void Main2()
+    static void Main()
     { 
         System.Console.WriteLine(num);
-        System.Console.WriteLine(Demo3.num);
+        System.Console.WriteLine(Demo.num);
         System.Console.ReadLine();
     }
 }
