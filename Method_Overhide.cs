d201fb22f3a45ad0c1fcd2c54360543882037c67
Rafe786 <110394841+Rafe786@users.noreply.github.com>
2022-08-23 05:39:38 -0700

    Update Method_Overhide.cs

--- Method_Overhide.cs
+++ Method_Overhide.cs
@@ -1,20 +1,20 @@
 ﻿//"new" keyword is used to hide a method, property, indexer,or event of base class into derived class.
 //"new" keyword is an optional.
 using System;
-class Parent11
+class Parent
 {
     protected static void Marry()
     {
         Console.WriteLine("Father Choice");
     }
 }
-class Child:Parent11
+class Child:Parent
 {
     protected static new void Marry()
     {
         Console.WriteLine("Children................ Choice");
     }
-    static void Main5()
+    static void Main()
     {
         //Child c = new Child();
         Marry();
