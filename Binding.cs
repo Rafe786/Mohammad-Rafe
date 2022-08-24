fa19737b80dc2c8c7f3eeb9863940d9957b219a5
Rafe786 <110394841+Rafe786@users.noreply.github.com>
2022-08-24 17:23:04 +0530

    Add files via upload

---
+++ Binding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Test
+{
+    public virtual void Show()
+    {
+        Console.WriteLine("Hello");
+    }
+}
+class Mix : Test
+{
+    public override void Show()
+    {
+        Console.WriteLine("Hi");
+    }
+    static void Main()
+    {
+        Test t = new Mix();
+        t.Show();
+    }
+}
---
+++ Binding2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Test
+{
+    public virtual void Process()
+    {
+        Console.WriteLine("2000");
+    }
+    public static void Display(Test test)
+    {
+        test.Process();
+    }
+}
+class Mix : Test
+{
+    public override void Process()
+    {
+        Console.WriteLine("2010");
+    }
+}
+class Demo
+{ 
+    static void Main()
+    {
+        Test t = new Test();
+        Test.Display(t);
+        Mix m = new Mix();
+        Test.Display(m);
+    }
+}
