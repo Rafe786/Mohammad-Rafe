0e2051b111d97b97e88422e60173bc414107633e
Mohammad Rafe <110394841+Rafe786@users.noreply.github.com>
2022-08-26 04:26:32 -0700

    Setter & Getter

---
+++ Company.cs
@@ -0,0 +1,44 @@
+﻿//Use Getter & Setter...................
+using System;
+namespace ConsoleApp4
+{
+    class Chetu
+    {
+        static void Main()
+        {
+            Console.Write("Enter The Employee Number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Enter The Employee Name : ");
+            string ename = Console.ReadLine();
+            Console.Write("Enter The Employee Salery : ");
+            double sal = Convert.ToDouble(Console.ReadLine());
+            Employee employee= new Employee();
+            employee.SetEmpNo(num);
+            employee.SetEmpName(ename);
+            employee.SetEmpSal(sal);
+            Console.WriteLine("............................................");
+
+            Console.Write("Enter The House Number: ");
+            int no = int.Parse(Console.ReadLine());
+            Console.Write("Enter The House Name : ");
+            string hname = Console.ReadLine();
+            Console.Write("Enter The PinCode : ");
+            int pin = int.Parse(Console.ReadLine());
+
+            Address address = new Address();
+            address.SetHouseNo(no);
+            address.SetHouseName(hname);
+            address.SetPinCode(pin);
+           employee.SetEmpAddress(address);
+            
+            Console.WriteLine("..............................................");
+            Console.WriteLine("................Employee Datials..............");
+            Console.WriteLine("No is : " + employee.GetEmpNo());
+            Console.WriteLine("Name is : " + employee.GetEmpName());
+            Console.WriteLine("Salery is : " + employee.GetEmpSal());
+            Console.WriteLine("..............................................");
+
+            Console.WriteLine("Address is: " + employee.GetEmpAddress());
+        }
+    }
+}
