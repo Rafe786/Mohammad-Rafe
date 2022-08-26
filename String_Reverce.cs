1e3f8370fe369dc90ecdf76b86256d1b5ca2a298
Mohammad Rafe <110394841+Rafe786@users.noreply.github.com>
2022-08-26 04:02:03 -0700

    Add files via upload

---
+++ String_Reverce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleApp5
+{
+    class Demo
+    {
+        static void Main()
+        {
+            string name = "ADDRESS";
+            string str = " ";
+            int length = 0;
+            length = name.Length - 1;
+            while (length >= 0)
+            {
+                str = str + name[length];
+                length--;
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
