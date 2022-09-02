e5bc21bc761ce0f9457b241d5ef2722733be5939
Mohammad Rafe <110394841+Rafe786@users.noreply.github.com>
2022-09-02 17:52:02 +0530

    Add files via upload

---
+++ PelindromeNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class PelindromeNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Number : ");
+            int num = int.Parse(Console.ReadLine());
+            int res, temp, sum = 0;
+            temp = num;
+            while (num > 0)
+            {
+                res = num % 10;                
+                sum = sum * 10 + res;
+                num = num / 10;
+            }
+            if (temp == sum)
+                Console.WriteLine($"{temp} is Pelindrome Number");
+            else
+                Console.WriteLine($"{temp} is Not Pelindrome Number ");
+        }
+    }
+}
---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class ReverseNumber
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter The Number ");
+            int num = int.Parse(Console.ReadLine());
+            int r, reverse = 0;
+            while(num!=0)
+            {
+                r = num % 10;
+                reverse = reverse * 10 + r;
+                num = num / 10;
+            }
+                Console.WriteLine("Reverse Number is : " + reverse);
+            
+        }
+    }
+}
